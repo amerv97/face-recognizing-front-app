4534e7c62dc4d49510c92a86928a95a1c4fb77d8
Amer Varajic <varajicamer@gmail.com>
2023-01-16 01:14:49 +0100

    Navigation, Logo, Rank, Image, Particles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Navigation/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank'
+import ParticlesBg from 'particles-bg';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesBg className='particles'
+      color='' type="square" bg={true} 
+      params={particlesOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+                {'Amer Varajic'}
+            </div>
+            <div className="white f1">
+                {'“The life of spies is to know, not bee known.”'}
+            </div>
+
+        </div>
+    )
+}
+
+export default Rank
